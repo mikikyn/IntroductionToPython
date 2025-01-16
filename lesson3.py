b5f4358e218cbf182cab265b1b23a983f58bbf6f
mikikyn <baizhigit.akaev@gmail.com>
2025-01-16 09:57:45 +0600

    ls5

---
+++ lesson3.py
@@ -0,0 +1,26 @@
+# c = float(input("Enter a number: "))
+# f = 12.5 // 10.2 * c + 35.1
+# print("The result of f = c + 15 is:", f)
+#
+# c = input('enter the celcium: ')
+# c = '10'
+# f = 9 / 5 * c + 32
+
+
+# num1 = float(input("Enter a number: "))
+# num2 = int(input("Enter another number: "))
+#
+# result_float = num1 / num2
+# result_int = num1 // num2
+#
+# print(f"Float division result of {num1} / {num2}", float(result_float))
+# print(f"Integer division result of {num1} // {num2}", int(result_int))
+
+
+
+
+
+
+
+
+
---
+++ lesson5.py
@@ -0,0 +1,8 @@
+age = int(input("enter you're age: "))
+
+age = 11
+myAge = str(age)
+age = 11.0
+
+print(age)
+print(type(age))
